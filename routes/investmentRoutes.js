6c81e41ff98a40f0e1694a424ecc1deb75a8b7e4
Chuci Qin <qin@yummy.ninja>
2023-07-04 17:38:42 -0400

    6

--- routes/investmentRoutes.js
+++ routes/investmentRoutes.js
@@ -3,20 +3,25 @@ const mongoose = require('mongoose');
 const Investment = mongoose.model('investments');
   
 module.exports = app => {
-  app.post('/api/investment', async (req, res) => {
-    const { investorPublicKey, referrerPublicKey, investmentAmount } = req.body;
+app.get('/api/investment', async (req, res) => {
+  try {
+    console.log('Received request:', req.query);
+
+    const { investorPublicKey, referrerPublicKey, investmentAmount } = req.query;
 
     const investment = new Investment({
       investorPublicKey,
       referrerPublicKey,
-      investmentAmount
+      investmentAmount: parseFloat(investmentAmount)
     });
 
-    try {
-      await investment.save();
-      res.send({ status: 'success' });
-    } catch (err) {
-      res.status(422).send(err);
-    }
-  });
+    await investment.save();
+    res.send({ status: 'success' });
+  } catch (err) {
+    console.error('Error handling request:', err);
+    res.status(500).send(err);
+  }
+});
+
+
 };
